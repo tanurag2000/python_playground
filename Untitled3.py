df95dabf301280ee04da4209a0878637ab14d34e
Anurag Tiwari <42093969+tanurag2000@users.noreply.github.com>
2021-07-25 19:54:18 +0530

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+l= []
+n=int(input("Enter number of elements : "))
+for i in range(n):
+    a=int(input())
+    l.append(a)
+l=tuple(l)
+
+
+# In[3]:
+
+
+l
+
+
+# In[ ]:
+
+
+
+
